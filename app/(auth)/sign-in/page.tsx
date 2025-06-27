310783268c078c53444470c9440c059c4c4bf11b
SahayakSharma <sahayaksharma6@gmail.com>
2025-06-27 16:16:28 +0530

    fix: style singin poage

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,18 +2,27 @@
 import { useAuth } from "@/context/authContext";
 import { useTheme } from "@/context/themeContext"
 import { FcGoogle } from "react-icons/fc";
+import { Poppins } from 'next/font/google';
+
+export const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['400', '500', '700'],
+    variable: '--font-poppins',
+});
+
 
 export default function Page() {
     const {initGoogleSignin}=useAuth();
     const {colors}=useTheme();
     return (
-        <div className='w-full h-screen flex justify-center items-center'>
+        <div className={`w-full h-screen flex justify-center items-center ${poppins.className}`}>
             <section className="rounded-md ">
+                <p className="text-2xl font-normal text-center pb-10">Login to Your Account</p>
                 <div className="flex gap-5 px-10 py-3 rounded-xl font-bold text-xl cursor-pointer" style={{backgroundColor:colors.primary,color:colors.text}}
                     onClick={()=>initGoogleSignin()}
                 >
                     <FcGoogle size={30}/>
-                    <p>SignIn With Gmail Account</p>
+                    <p className="text-white">SignIn With Gmail Account</p>
                 </div>
             </section>
         </div>
